369e00b6269f851db3bf800b0c6bc2bfb912f1ff
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-10-21 11:49:06 -0400

    Methods

--- todo-server/src/main/java/com/codedifferently/todoserver/todo/ToDoController.java
+++ todo-server/src/main/java/com/codedifferently/todoserver/todo/ToDoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,12 +23,57 @@ public class ToDoController {
 
     //Todo: Create a Get Method that returns all items with a status code of 200
 
+    @GetMapping("/allToDos")
+    public ResponseEntity<List<ToDo>> getAllToDos(){
+
+        HttpStatus status = HttpStatus.OK;
+
+        List<ToDo> todos = toDoService.getAll();
+
+        return new ResponseEntity<List<ToDo>>(status);
+
+
+    }
+
     //Todo: Create a POST Method that creates an item with a status code of 201
+
+    @PostMapping("/createToDo")
+    public ResponseEntity<ToDo> createToDo(@RequestBody ToDo todo){
+
+        HttpStatus status = HttpStatus.CREATED;
+
+        ToDo created = toDoService.create(todo.getText());
+
+        return new ResponseEntity<>(created, status);
+
+
+    }
 
     //Todo: Create a PUT Method that updates a status code with 204
 
+    @PutMapping("/updateToDo")
+    public ResponseEntity<Void> updateToDo(@PathVariable("id") Integer id, @RequestBody ToDo toDo){
+
+        HttpStatus status = HttpStatus.NO_CONTENT;
+
+        toDoService.toggleComplete(id);
+
+        return new ResponseEntity<Void>(status);
+
+    }
 
     //Todo: Create a DELETE method that removes the item and returns a status code of 202
 
+    @DeleteMapping("/deleteToDo")
+    public ResponseEntity<ToDo> deleteToDoById(@PathVariable("id") Integer id){
+
+        HttpStatus status = HttpStatus.ACCEPTED;
+
+        toDoService.remove(id);
+
+        return new ResponseEntity<>(status);
+
+
+    }
 
 }
--- todo-server/src/test/java/com/codedifferently/todoserver/todo/ToDoControllerTest.java
+++ todo-server/src/test/java/com/codedifferently/todoserver/todo/ToDoControllerTest.java
@@ -1,4 +1,98 @@
 package com.codedifferently.todoserver.todo;
 
+import net.minidev.json.JSONUtil;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = ToDoController.class)
 public class ToDoControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @MockBean
+    private ToDoService toDoService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup(){
+        // creating a clean container to put data in
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void getAllToDosTest() throws Exception {
+
+        List<ToDo> toDoList = new ArrayList<>();
+        toDoList.add(ToDo.builder("Study Core Java"));
+        toDoList.add(ToDo.builder("Practice Code Problems"));
+        toDoList.add(ToDo.builder("Sleep , Sleep, Sleep"));
+
+        BDDMockito.given(toDoService.getAll()).willReturn(toDoList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/todos/allToDos"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void createToDosTest() throws Exception{
+
+        ToDo toDo = ToDo.builder("Brush Up On Spring");
+
+        BDDMockito.given(toDoService.create(Mockito.anyString())).willReturn(toDo);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/todos/createToDo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                .content(JSONUtil.toJson(todo)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.text", CoreMatchers.is("Look more into SpringBoot")));
+    }
+
+    @Test
+    public void updateToDoTest() throws Exception{
+
+        ToDo toDo = ToDo.builder("Watch Videos");
+
+        BDDMockito.given(toDoService.toggleComplete(toDo.getId())).willReturn(toDo));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/todos/all/updateToDo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JSONUtil.toJson(toDo)))
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andReturn();
+    }
+
+    @Test
+    public void deleteToDoTest() throws Exception{
+
+        ToDo toDo = ToDo.builder("Read Chapter One");
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/todos/all/deleteToDo"))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andReturn();
+    }
+
+
+
+
 }
